cec9bc326cda63a2966b5e55a288c4788b75d9d3
daniyar135 <94024241+daniyar135@users.noreply.github.com>
2022-04-25 12:43:28 -0500

    Add files via upload

--- Dashboard.py
+++ Dashboard.py
@@ -5,15 +5,17 @@ import pandas as pd
 import os
 
 df1 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/icebreaker/master/Icebreaker_stats_final.csv')
-df2 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/icebreaker/master/SV.CSV')
-df3 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/icebreaker/master/Silence.csv')
+df2 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/icebreaker/master/Silence.csv')
+df3 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/icebreaker/master/SV.CSV')
+df4 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/icebreaker/master/Top10.csv')
 
 app = Dash(__name__)
 server = app.server
 figa = px.scatter(df1, y="Call duration", x="Number of silences", color="Recording ID", size="Silence Max")
-figs = px.bar(df3, y="Silence duration", x="Recording ID")
-figb = px.bar(df3, y="Recording ID", x="Class")
-figsv = px.histogram(df2, y="Contact ID", x="Call Duration")
+figs = px.bar(df2, y="Silence duration", x="Contact ID")
+figb = px.bar(df2, y="Contact ID", x="Class")
+figsv = px.histogram(df3, y="Contact ID", x="Call duration")
+figtop = px.histogram(df4, y="Contact ID", x="Call duration")
 
 app.layout = html.Div(children=[
     html.H1(children='Icebreaker Dashboard'),
@@ -36,7 +38,7 @@ app.layout = html.Div(children=[
         df1['week'].max(),
         step=None,
         value=df1['week'].min(),
-        marks={str(week): ("week" + str(week)) for week in df1['week'].unique()},
+        marks={str(week): ("week" + str(week)) for week in df1['week'].unique()} + df1['week'],
         id='week-slider'
     )
     ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
@@ -62,6 +64,11 @@ app.layout = html.Div(children=[
     dcc.Graph(
     id='sv-graph',
     figure=figsv
+    ),
+
+    dcc.Graph(
+    id='top10-graph',
+    figure=figtop
     )
 
 ])
@@ -79,12 +86,12 @@ def update_figure(selected_week):
                      log_x=True, size_max=55)
     fig.update_layout(transition_duration=500)
 
-    filtered_df3 = df3[df3.week == selected_week]
+    filtered_df2 = df2[df2.week == selected_week]
 
-    fig1 = px.bar(filtered_df3, y="Silence duration", x="Recording ID")
+    fig1 = px.bar(filtered_df2, y="Silence duration", x="Recording ID")
     fig1.update_layout(transition_duration=500)
 
-    fig2 = px.bar(filtered_df3, y="Recording ID", x="Class")
+    fig2 = px.bar(filtered_df2, y="Recording ID", x="Class")
     fig2.update_layout(transition_duration=500)
 
     return fig, fig1, fig2
