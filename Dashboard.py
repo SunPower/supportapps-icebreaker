ee5d2d5d387d7fb4cbe4cfcc6659bc8075063642
daniyar135 <94024241+daniyar135@users.noreply.github.com>
2022-04-26 13:46:20 -0500

    Add files via upload

--- Dashboard.py
+++ Dashboard.py
@@ -4,41 +4,48 @@ import plotly.express as px
 import pandas as pd
 import os
 
-df1 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/icebreaker/master/Icebreaker_stats_final.csv')
-df2 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/icebreaker/master/Silence.csv')
-df3 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/icebreaker/master/SV.CSV')
-df4 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/icebreaker/master/Top10.csv')
+df1 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/supportapps-icebreaker/master/Icebreaker_stats_final.csv')
+df2 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/supportapps-icebreaker/master/Silence.csv')
+df3 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/supportapps-icebreaker/master/SV.csv')
+df4 = pd.read_csv('https://raw.githubusercontent.com/daniyar135/supportapps-icebreaker/master/Top10.csv')
 
 app = Dash(__name__)
 server = app.server
-figa = px.scatter(df1, y="Call duration", x="Number of silences", color="Recording ID", size="Silence Max")
-figs = px.bar(df2, y="Silence duration", x="Contact ID")
-figb = px.bar(df2, y="Contact ID", x="Class")
-figsv = px.histogram(df3, y="Contact ID", x="Call duration")
-figtop = px.histogram(df4, y="Contact ID", x="Call duration")
+figa = px.scatter(df1, y="Call duration", x="Number of silences", color="Contact ID", size="Silence Max")
+figs = px.bar(df2, y="Silence duration (seconds)", x="Contact ID")
+figb = px.bar(df2, y="Contact ID", x="Class", color="Subclass")
+figsv = px.bar(df3, y="callduration", x="Date")
+figtop = px.bar(df4, y="callduration", x="Contact Start Date Time", color="Skill Name")
 
 app.layout = html.Div(children=[
     html.H1(children='Icebreaker Dashboard'),
 
     html.Div(children='''
-        Visualizing data from our longest SunVault related calls. The longest silences are represented in a bigger shape.
+        Visualizing data from our longest SunVault related calls.
     '''),
     html.Div(children='''
-        Use the slider at the bottom to see each week's longest calls that had silences > 2 minutes.
+        Use the slider below to see each week's longest SunVault related calls that had silences > 2 minutes. Larger shapes represent longer silences.
+    '''),
+    html.Div(children='''
+        The upper graph on the right represents the type of issue that prompted the silence.
+    '''),
+    html.Div(children='''
+        The lower graph on the right represents the length of the silence for each call.
     '''),
 
     html.Div([
 
     dcc.Graph(
-    id='graph-with-slider'
+    id='graph-with-slider',
+    figure=figa
     ),
 
     dcc.Slider(
-        df1['week'].min(),
-        df1['week'].max(),
+        min=df1['week'].min(),
+        max=df1['week'].max(),
         step=None,
         value=df1['week'].min(),
-        marks={str(week): ("week" + str(week)) for week in df1['week'].unique()} + df1['week'],
+        marks={str(week): ("week" + str(week)) for week in df1['week'].unique()},
         id='week-slider'
     )
     ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
@@ -56,14 +63,62 @@ app.layout = html.Div(children=[
 
     ], style={'display': 'inline-block', 'width': '49%'}),
 
+    html.Div(children='''
+        The graph below represents all of the longest SunVault related calls for 2022 (5 per week)
+    '''),
+
     dcc.Graph(
     id='alltime-graph',
     figure=figa
     ),
 
+    html.Div(children='''
+        The histogram below represents the call duration for all SunVault related calls for 2022.
+    '''),
+
+    dcc.Slider(
+        min=df3['callduration'].min(),
+        max=df3['callduration'].max(),
+        step=None,
+        marks={
+        0: {'label': 'all', 'style': {'color': '#77b0b1'}},
+        60: {'label': '>1hr'},
+        120: {'label': '>2hr'},
+        180: {'label': '>3hr'},
+        240: {'label': '>4hr'},
+        300: {'label': ">5hr", 'style': {'color': '#f50'}},
+        360: {'label': ">6hr", 'style': {'color': '#f50'}},
+        },
+        value=df3['callduration'].min(),
+        #tooltip={"placement": "bottom", "always_visible": True},
+        id='sv-slider'
+    ),
+
     dcc.Graph(
     id='sv-graph',
     figure=figsv
+    ),
+
+    html.Div(children='''
+        The histogram below represents the call duration for ALL calls for 2022 (top 10 skills including SunVault).
+    '''),
+
+    dcc.Slider(
+        min=df4['callduration'].min(),
+        max=df4['callduration'].max(),
+        step=None,
+        marks={
+        0: {'label': 'all', 'style': {'color': '#77b0b1'}},
+        60: {'label': '>1hr'},
+        120: {'label': '>2hr'},
+        180: {'label': '>3hr'},
+        240: {'label': '>4hr'},
+        300: {'label': ">5hr", 'style': {'color': '#f50'}},
+        360: {'label': ">6hr", 'style': {'color': '#f50'}},
+        },
+        value=df4['callduration'].min(),
+        #tooltip={"placement": "bottom", "always_visible": True},
+        id='top10-slider'
     ),
 
     dcc.Graph(
@@ -77,26 +132,45 @@ app.layout = html.Div(children=[
     Output('graph-with-slider', 'figure'),
     Output('blocker-graph', 'figure'),
     Output('silence-graph', 'figure'),
-    Input('week-slider', 'value'))
+    [Input('week-slider', 'value')])
 
 def update_figure(selected_week):
-    filtered_df = df1[df1.week == selected_week]
 
-    fig = px.scatter(filtered_df, x="Number of silences", y="Call duration", size="Call duration", color="Recording ID",
+    filtered_df = df1[df1.week == selected_week]
+    fig = px.scatter(filtered_df, x="Number of silences", y="Call duration", size="Silence Max", color="Contact ID",
                      log_x=True, size_max=55)
     fig.update_layout(transition_duration=500)
 
     filtered_df2 = df2[df2.week == selected_week]
-
-    fig1 = px.bar(filtered_df2, y="Silence duration", x="Recording ID")
+    fig1 = px.bar(filtered_df2, y="Silence duration (seconds)", x="Contact ID")
     fig1.update_layout(transition_duration=500)
-
-    fig2 = px.bar(filtered_df2, y="Recording ID", x="Class")
+    fig2 = px.bar(filtered_df2, y="Contact ID", x="Class", color="Subclass")
     fig2.update_layout(transition_duration=500)
 
     return fig, fig1, fig2
 
+@app.callback(
+    Output('sv-graph', 'figure'),
+    [Input('sv-slider', 'value')])
+
+def update_figure2(selected_duration):
+
+    filtered_df3 = df3[df3.callduration >= selected_duration]
+    fig3 = px.bar(filtered_df3, y="callduration", x="Date")
+    fig3.update_layout(transition_duration=500)
+    return fig3
+
+@app.callback(
+    Output('top10-graph', 'figure'),
+    [Input('top10-slider', 'value')])
+
+def update_figure3(selected_duration):
+
+    filtered_df4 = df4[df4.callduration >= selected_duration]
+    fig4 = px.bar(filtered_df4, y="callduration", x="Contact Start Date Time", color="Skill Name")
+    fig4.update_layout(transition_duration=500)
+    return fig4
+
 if __name__ == '__main__':
     app.run_server()
-
 # %%
